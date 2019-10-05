f2e8fd4d16d9ebc6fd8c2ec4c2ef08d0d004b6dd
Michael Phung <mikey@phungmi3.local>
2019-10-05 13:11:51 -0400

    added animations to landing page

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App-header">
       <header>
-        <h1 className="App-title">
+        <h1 className="App-title" id="title">
           <p className="App-register">login/register</p>
           <span style={{color:'black'}}>grade</span>
           <span style={{color:'white'}}>.io</span>
         </h1>
       </header>
-      <header className="App-stay-on-track">
+      <header className="App-stay-on-track" id="track">
           Stay on track of your grades
       </header>
       <button className="button">Get Started</button>
