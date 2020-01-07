7c6cca0583b6f11469197443e7f49bffa29ff5a2
Jarrod Servilla <jarrod.servilla@gmail.com>
2020-01-06 21:24:37 -0500

    Base version of navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,25 +1,110 @@
-import React from 'react';
+import React from "react";
 import "./nav.css";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import Collapse from "@material-ui/core/Collapse";
+import Image from "react-bootstrap/Image";
 
+function SidebarItem({ depthStep = 10, depth = 0, expanded, item, ...rest }) {
+  const [collapsed, setCollapsed] = React.useState(true);
+  const { label, items, Icon, onClick: onClickProp } = item;
 
-const Nav = props => {
-    let navopen;
-    if(props.show) {
-        navopen = "nav-anim"
+  function toggleCollapse() {
+    setCollapsed(prevValue => !prevValue);
+  }
+
+  function onClick(e) {
+    if (Array.isArray(items)) {
+      toggleCollapse();
     }
-    else {
-        navopen = "nav"
+    if (onClickProp) {
+      onClickProp(e, item);
     }
+  }
 
-    return (
-        <div className={navopen}>
-            <ul className="nav-ul">
-                <div>
-                    2019/2020
-                </div>
-            </ul>
+  let expandIcon;
+
+  if (Array.isArray(items) && items.length) {
+    expandIcon = !collapsed ? (
+      <ExpandLessIcon
+        className={
+          "sidebar-item-expand-arrow" + " sidebar-item-expand-arrow-expanded"
+        }
+      />
+    ) : (
+      <ExpandMoreIcon className="sidebar-item-expand-arrow" />
+    );
+  }
+
+  return (
+    <>
+      <ListItem
+        className="sidebar-item"
+        onClick={onClick}
+        button
+        dense
+        {...rest}
+      >
+        <div className="image">
+          <Image src="/Assets/arrow.png" fluid />
         </div>
-    )
+        <div
+          style={{ paddingLeft: depth * depthStep }}
+          className="sidebar-item-content"
+        >
+          {Icon && <Icon className="sidebar-item-icon" fontSize="small" />}
+
+          <div className="sidebar-item-text">{label}</div>
+        </div>
+        {expandIcon}
+      </ListItem>
+      <Collapse in={!collapsed} timeout="auto" unmountOnExit>
+        {Array.isArray(items) ? (
+          <List disablePadding dense>
+            {items.map((subItem, index) => (
+              <React.Fragment key={`${subItem.name}${index}`}>
+                {subItem === "divider" ? (
+                  <Divider style={{ margin: "6px 0" }} />
+                ) : (
+                  <SidebarItem
+                    depth={depth + 1}
+                    depthStep={depthStep}
+                    item={subItem}
+                  />
+                )}
+              </React.Fragment>
+            ))}
+          </List>
+        ) : null}
+      </Collapse>
+    </>
+  );
+}
+
+function Nav({ items, depthStep, depth, expanded }) {
+  return (
+    <div className="sidebar">
+      <List disablePadding dense>
+        {items.map((sidebarItem, index) => (
+          <React.Fragment key={`${sidebarItem.name}${index}`}>
+            {sidebarItem === "divider" ? (
+              <Divider style={{ margin: "6px 0" }} />
+            ) : (
+              <SidebarItem
+                depthStep={depthStep}
+                depth={depth}
+                expanded={expanded}
+                item={sidebarItem}
+              />
+            )}
+          </React.Fragment>
+        ))}
+      </List>
+    </div>
+  );
 }
 
 export default Nav;
